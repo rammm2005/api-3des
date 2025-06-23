ac8362c0aceeb8676be4b636e68559cc0f18b0e0
marmutMerahJambu <119766108+rammm2005@users.noreply.github.com>
2025-06-23 19:42:55 +0800

    fix cors on fe

--- src/server.js
+++ src/server.js
@@ -11,7 +11,9 @@ import authRoutes from './routes/auth.js';
 import { encrypt3DES } from './lib/crypto.js';
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  { origin: ["https://3des-chatapp.vercel.app/", "http://localhost:3000"], },
+));
 app.use(express.json());
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true, limit: '10mb' }))
